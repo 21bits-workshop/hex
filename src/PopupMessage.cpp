07ccc276ef4841da684108e21a53aac717bf2cf7
21bits-workshop <takomo@21b.space>
2026-05-26 22:51:17 -0400

    Prettified Popupmessage with a transparent background.

--- src/PopupMessage.cpp
+++ src/PopupMessage.cpp
@@ -1,5 +1,6 @@
 #include "PopupMessage.h"
 #include "Colors.h"
+#include <SDL2/SDL_blendmode.h>
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_render.h>
@@ -16,16 +17,26 @@ PopupMessage::PopupMessage(int startX, int startY, int width, int height,
 void PopupMessage::render(SDL_Renderer *renderer) {
   SDL_Surface *popupSurface =
       SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
+  SDL_Rect popupSurfaceDestRect = {0, 0, 0, 0};
+  SDL_SetSurfaceBlendMode(popupSurface, SDL_BLENDMODE_BLEND);
+  SDL_SetSurfaceAlphaMod(popupSurface, 128);
+
   SDL_Surface *popupText = TTF_RenderText_Blended(font, message, Colors::white);
   SDL_Rect textDestRect = {0, 0, 0, 0};
-  SDL_BlitSurface(popupText, NULL, popupSurface, &textDestRect);
 
-  SDL_Texture *messageTexture =
+  SDL_Texture *popupTexture =
       SDL_CreateTextureFromSurface(renderer, popupSurface);
   SDL_Rect popupDestRect = {startX, startY, width, height};
-  SDL_RenderCopy(renderer, messageTexture, NULL, &popupDestRect);
+  SDL_RenderCopy(renderer, popupTexture, NULL, &popupDestRect);
 
-  SDL_DestroyTexture(messageTexture);
+  SDL_Texture *textTexture = SDL_CreateTextureFromSurface(renderer, popupText);
+  int textW, textH;
+  SDL_QueryTexture(textTexture, NULL, NULL, &textW, &textH);
+  SDL_Rect popupTextRect = {startX + 2, startY + 2, textW, textH};
+  SDL_RenderCopy(renderer, textTexture, NULL, &popupTextRect);
+
+  SDL_DestroyTexture(textTexture);
+  SDL_DestroyTexture(popupTexture);
   SDL_FreeSurface(popupText);
   SDL_FreeSurface(popupSurface);
 }
