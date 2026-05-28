8e86dd733b6c96b0ffe7347b07f5337dfd3a02ef
21bits <takomo@21b.space>
2026-05-28 11:03:47 -0400

    Update Entity.cpp

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -4,7 +4,6 @@
 Entity::Entity(int x, int y, char displayCharacter, SDL_Color fgColor)
     : x(x), y(y), displayCharacter(displayCharacter), fgColor(fgColor) {}
 
-// Getters & Setters
 int Entity::getX() const { return x; }
 int Entity::getY() const { return y; }
 char Entity::getDisplayCharacter() const { return displayCharacter; }
